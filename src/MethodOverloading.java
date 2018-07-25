137f2a9a068e2c0fc3cfef3f0bf005c4e475d450
runesam <same7.hamada@gmail.com>
2018-07-25 12:05:09 +0200

    bugs

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -1,22 +1,28 @@
 public class MethodOverloading {
     public static void main (String[] args) {
         int scoreI = calculateScore("Sam", 500);
-        int scoreII = calculateScore(500);
-        double result = calcFeetAndInchesToCentimeters(7,5);
+        int scoreII = calculateScore("Cherie", 200);
+        int scoreIII = calculateScore(500);
+        int scoreIV = calculateScore(300);
+        double resultI = calcFeetAndInchesToCentimeters(7,5);
+        double resultII = calcFeetAndInchesToCentimeters(10,4);
         System.out.println(scoreI);
         System.out.println(scoreII);
-        System.out.println(result);
+        System.out.println(scoreIII);
+        System.out.println(scoreIV);
+        System.out.println(resultI);
+        System.out.println(resultII);
     }
-    public static int calculateScore(String playerName, int score) {
+    private static int calculateScore(String playerName, int score) {
         System.out.println("player " + playerName + " scored " + score + " points.");
         return score * 1000;
     }
-    public static int calculateScore(int score) {
+    private static int calculateScore(int score) {
         System.out.println("player with no name"+ " scored " + score + " points.");
         return score * 1000;
     }
-    public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
-        if (feet >= 0 || (inches >= 0 && inches <= 12)) {
+    private static double calcFeetAndInchesToCentimeters(int feet, int inches) {
+        if (feet < 0 || !(inches >= 0 && inches <= 12)) {
             return -1;
         }
         double centimeters = (feet * 12) * 2.54;
