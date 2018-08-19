416966b6de46cb6c8629af3a71444123f88c54bd
runesam <same7.hamada@gmail.com>
2018-08-19 17:15:34 +0200

    parsing

---
+++ src/Parsing.java
@@ -0,0 +1,6 @@
+public class Parsing {
+    public static void main(String [] args) {
+        System.out.println(Integer.parseInt("2018") + 1);
+        System.out.println(Double.parseDouble("2018.123123") + 0.2);
+    }
+}
